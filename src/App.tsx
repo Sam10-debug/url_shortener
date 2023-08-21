ae17cbb8650fc41e4d2e843de68c69595accab27
Samuel <emorinkensam@gmail.com>
2023-08-21 08:15:31 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Nav from './components/Nav'
+
+function App() {
+
+  return (
+    <main>
+      <Nav />
+      <Hero />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
